51b5e21e4ee34f100c14e3f6754ffda92e766738
shibuya-bitvalley <ryu.j.shibu@gmail.com>
2018-02-27 16:02:37 +0900

    [mod]front angle add

--- rab_bringup/src/joy_avoidance.cpp
+++ rab_bringup/src/joy_avoidance.cpp
@@ -24,24 +24,17 @@ class JoyAvoidance{
    ros::Publisher pub_vel;
    ros::Publisher pub_flag;
    
-   // param for topic
-   //std::string sub_cmdvel_topic_;
-   //std::string scan_topic_;
-   //std::string pub_cmdvel_topic_;
-   
    // declaration of menber variable
-   int front;
-   int right;
-   int left;
+   int right_front;
+   int right_back;
+   int left_front;
+   int left_back;
    int flag;
    std_msgs::Int8 flag_states;
 };
 
 JoyAvoidance::JoyAvoidance(ros::NodeHandle &nh){
    ros::NodeHandle n("~");
-   //n.param<std::string>("sub_cmdvel_topic", sub_cmdvel_topic_, "/cmd_vel_old");
-   //n.param<std::string>("scan_topic", scan_topic_, "/base_scan");
-   //n.param<std::string>("pub_cmdvel_topic", pub_cmdvel_topic_, "/cmd_vel");
    // Subscriber
    sub_scan = nh.subscribe<sensor_msgs::LaserScan>(n.param<std::string>("scan_topic","base_scan"), 1, &JoyAvoidance::sensorCallback, this);
    sub_vel = nh.subscribe<geometry_msgs::Twist>(n.param<std::string>("sub_cmdvel_topic","cmd_vel_old"), 1, &JoyAvoidance::cmdvelCallback, this);
@@ -51,7 +44,7 @@ JoyAvoidance::JoyAvoidance(ros::NodeHandle &nh){
 }
 
 double JoyAvoidance::minimumdist(const sensor_msgs::LaserScan::ConstPtr& scan, 
-			   double angle_1, double angle_2, double min_dist, double max_dist){
+				 double angle_1, double angle_2, double min_dist, double max_dist){
    double distance = 50.0;
    for(int i = angle_1; i <= angle_2; i++){
       if(min_dist < scan->ranges[i] && scan->ranges[i] < max_dist){
@@ -63,22 +56,26 @@ double JoyAvoidance::minimumdist(const sensor_msgs::LaserScan::ConstPtr& scan,
    }else{
       return 50.0;
    }
- }
+}
 
 void JoyAvoidance::sensorCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
    // 位置に応じてフラグ変更
-   this->front = scan->ranges.size() / 2; // 正面方向
-   this->right = scan->ranges.size() * 3 / 8; // 右45度方向
-   this->left = scan->ranges.size() * 5 / 8; // 左45度方向
-   double right_dist = minimumdist(scan, right, front, 0.0, 2.0);
-   double left_dist = minimumdist(scan, front, left, 0.0 ,2.0);
+   this->right_front = scan->ranges.size() * 3 / 8; // 右45度前方
+   this->right_back = scan->ranges.size() * 1 / 8; // 右45度後方
+   this->left_front = scan->ranges.size() * 5 / 8; // 左45度前方
+   this->left_back = scan->ranges.size() * 7 / 8; // 左45度後方
+   double right_dist = minimumdist(scan, right_back, right_front, 0.0, 2.0);
+   double front_dist = minimumdist(scan, right_front, left_front, 0.0, 2.0);
+   double left_dist = minimumdist(scan, left_front, left_back, 0.0 ,2.0);
    int flag_angle;
-   if(right_dist < left_dist){
-      flag_angle = 1; //右舷前方に障害物あり
-   }else if(right_dist > left_dist){
-      flag_angle = 2;
+   if(right_dist < front_dist && right_dist < left_dist){
+      flag_angle = 1; // 右舷前方に障害物あり
+   }else if(left_dist < front_dist && left_dist < right_dist){
+      flag_angle = 2; // 左舷前方に障害物あり
+   }else if(front_dist < right_dist && front_dist < left_dist){
+      flag_angle = 3; // 正面に障害物あり
    }else{
-      flag_angle = 0;
+      flag_angle = 0; // 障害物なし
    }
    switch(flag_angle){
     case 1:
@@ -103,6 +100,17 @@ void JoyAvoidance::sensorCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
 	 flag = 4;
 	 break;
       }
+    case 3:
+      if(front_dist < 1.0){
+	 flag = 9;
+	 break;
+      }else if(front_dist < 1.5){
+	 flag = 8;
+	 break;
+      }else if(front_dist < 2.0){
+	 flag = 7;
+	 break;
+      }      
     case 0:
       flag = 0;
       break;
@@ -114,29 +122,30 @@ void JoyAvoidance::cmdvelCallback(const geometry_msgs::Twist::ConstPtr& vel){
    // フラグに応じてcmd_velの値を調整しをpublish
    geometry_msgs::Twist cmd_vel;
    // 右舷前方
-   if(flag == 1 && (vel->linear.x > 0.0 || vel->angular.z < 0.0)){
-      cmd_vel.linear.x = vel->linear.x * 0.8;
+   if(flag == 1 && (vel->linear.x > 0.0)){
       cmd_vel.angular.z = vel->angular.z * 0.8;
-   }else if(flag == 2 && (vel->linear.x > 0.0 || vel->angular.z < 0.0)){
-      cmd_vel.linear.x = vel->linear.x * 0.5;
+   }else if(flag == 2 && (vel->linear.x > 0.0)){
       cmd_vel.angular.z = vel->angular.z * 0.5;
-   }else if(flag == 3 && (vel->linear.x > 0.0 || vel->angular.z < 0.0)){
-      cmd_vel.linear.x = 0.0;
+   }else if(flag == 3 && (vel->linear.x > 0.0)){
       cmd_vel.angular.z = 0.0;
    }else{
-      cmd_vel.linear.x = vel->linear.x;
       cmd_vel.angular.z = vel->angular.z;
    }
    // 左舷前方
-   if(flag == 4 && (vel->linear.x > 0.0 || vel->angular.z > 0.0)){
-      cmd_vel.linear.x = vel->linear.x * 0.8;
+   if(flag == 4 && (vel->linear.x > 0.0)){
       cmd_vel.angular.z = vel->angular.z * 0.8;
-   }else if(flag == 5 && (vel->linear.x > 0.0 || vel->angular.z > 0.0)){
-      cmd_vel.linear.x = vel->linear.x * 0.5;
+   }else if(flag == 5 && (vel->linear.x > 0.0)){
       cmd_vel.angular.z = vel->angular.z * 0.5;
-   }else if(flag == 6 && (vel->linear.x > 0.0 || vel->angular.z > 0.0)){
-      cmd_vel.linear.x = 0.0;
+   }else if(flag == 6 && (vel->linear.x > 0.0)){
       cmd_vel.angular.z = 0.0;
+   }
+   // 正面
+   if(flag == 7 && (vel->linear.x > 0.0)){
+      cmd_vel.linear.x = vel->linear.x * 0.8;
+   }else if(flag == 8 && (vel->linear.x > 0.0)){
+      cmd_vel.linear.x = vel->linear.x * 0.5;
+   }else if(flag == 9 && (vel->linear.x > 0.0)){
+      cmd_vel.linear.x = 0.0;
    }
    cmd_vel.linear.y = 0.0;
    pub_vel.publish(cmd_vel);
