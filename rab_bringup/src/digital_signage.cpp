d6f3eb5bb08fa81122d30b67be120618caffb63c
shibuya-bitvalley <ryu.j.shibu@gmail.com>
2018-05-01 18:10:45 +0900

    [tempo]commit

--- rab_bringup/src/digital_signage.cpp
+++ rab_bringup/src/digital_signage.cpp
@@ -12,7 +12,7 @@ class DigitalSignage{
    
  private:
    int isInCircle(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg, double x, double y, double radius);
-   int isInAngle(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg, double z, double w);
+   int isInAngle(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg, double x, double y, double th);
    void odomCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg);
    void publish_image(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg);
 
@@ -43,7 +43,7 @@ class DigitalSignage{
 DigitalSignage::DigitalSignage(ros::NodeHandle &nh){
    // using parameter server
    ros::NodeHandle n("~");
-   flag = 0;
+   count = -1;
    
    // 画像読み込み１枚目
    n.param<std::string>("first_path", first_path_, ros::package::getPath("rab_bringup") + "/picture/sample.JPG");
@@ -71,8 +71,6 @@ int DigitalSignage::isInCircle(const geometry_msgs::PoseWithCovarianceStamped::C
    double dist;
    dist = sqrt((fabs(msg->pose.pose.position.x)-fabs(x)) * (fabs(msg->pose.pose.position.x)-fabs(x))
 	       + (fabs(msg->pose.pose.position.y)-fabs(y)) * (fabs(msg->pose.pose.position.y)-fabs(y)));
-   ROS_WARN("dist : %lf", dist);
-   ROS_WARN("radius : %lf", radius);
    if(dist < radius){
       return 0;
    }else{
@@ -80,75 +78,87 @@ int DigitalSignage::isInCircle(const geometry_msgs::PoseWithCovarianceStamped::C
    }
 }
 
-int DigitalSignage::isInAngle(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg, double z, double w){
-   // 角度の指定
+int DigitalSignage::isInAngle(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg, double z, double w, double th){
+   double euler = 2 * atan2(z, w);
+   ROS_INFO("Angle : %lf", euler);
+   if((th - M_PI / 4) < euler && euler < (th + M_PI / 4))
+     return 0;
+   else
+     return 1;
 }
 
 void DigitalSignage::publish_image(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg){
-   // 条件分岐
-   if(isInCircle(msg, first_x_, first_y_, 1.0) == 0)
+   // 画像表示
+   if(isInCircle(msg, first_x_, first_y_, 2.0) == 0  && isInAngle(msg, first_x_, first_y_, M_PI / 2) == 0)
      flag = 1;
-   else if(isInCircle(msg, second_x_, second_y_, 1.0) == 0)
+   else if(isInCircle(msg, second_x_, second_y_, 2.0) == 0 && isInAngle(msg, second_x_, second_y_, 0) == 0)
      flag = 2;
+   else
+     flag = 0;
    ROS_INFO("flag : %d", flag);
    
-   // 画像表示
-   if(count <= 0 && flag == 0){
+   switch(flag){
+    case 0:
       cv::imshow("Image", default_img);
       cv::waitKey(1);
-   }else if(count == 0 && flag == 1){	 
+      break;
+    case 1:
+      cv::imshow("Image", first_img);
+      cv::waitKey(1);
+      break;
+    case 2:
+      cv::imshow("Image", second_img);
+      cv::waitKey(1);
+      break;
+   }
+   
+   /*
+   if(count > 250 || count <= -1){
+      flag = 0;
+      cv::imshow("Image", default_img);
+      cv::waitKey(1);
+   }
+   if(flag <= 10 && isInCircle(msg, first_x_, first_y_, 2.0) == 0 
+      && isInAngle(msg, first_x_, first_y_, 180) == 0){
+      flag = 1;	 
       cv::imshow("Image", first_img);
       cv::waitKey(1);	  
-   }else if(count == 0 && flag == 2){
-      // 残り距離描画
-      double dist = sqrt((fabs(msg->pose.pose.position.x)-fabs(second_x_)) * (fabs(msg->pose.pose.position.x)-fabs(second_x_))
-	       + (fabs(msg->pose.pose.position.y)-fabs(second_y_)) * (fabs(msg->pose.pose.position.y)-fabs(second_y_)));
-      char dist_c[256];
-      sprintf(dist_c, "%lf", dist);
-      cv::putText(second_img, dist_c, cv::Point(20,100), cv::FONT_HERSHEY_SIMPLEX, 10, cv::Scalar(0,0,0), 3, CV_AA);
-		  cv::imshow("Image", second_img);
-      cv::waitKey(1);
+      count = 0;
+   }else if(flag <= 10 && isInCircle(msg, second_x_, second_y_, 2.0) == 0 
+	    && isInAngle(msg, second_x_, second_y_, 0) == 0){
+      flag = 1;	 
+      cv::imshow("Image", second_img);
+      cv::waitKey(1);	  
+      count = 0;
    }else{
+      flag = 1;
       count+=1;
-      if(count > 250)
-	count = 0;
-   }
-   ROS_WARN("count = %d", count);
+   }*/
+   //ROS_WARN("count = %d", count);
 }
 
 void DigitalSignage::odomCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg){
-   /*
-    double orientation_z = 0;
-    double orientation_w = 0;
-    double th0 = 0;
-    th0 = 2 * asin(msg->pose.pose.orientation.z / 2);
-    double th1 = 0;
-    th1 = 2 * acos(msg->pose.pose.orientation.w / 2);
-    ROS_INFO("Seq: [%d]", msg->header.seq);
-    */ 
-   ROS_INFO("Position-> x: [%lf], y: [%lf]", msg->pose.pose.position.x, msg->pose.pose.position.y);
-   //ROS_INFO("Orientation-> th0: [%lf], th1: [%lf]", th0 * 180 / 3.14, th1 * 180 / 3.14);
    publish_image(msg);
+   int hoge = DigitalSignage::
 }
 
 int DigitalSignage::check_image(){
    // 画像が読み込まれなかったら終了
    if(first_img.empty() || second_img.empty() || default_img.empty()){
-      ROS_ERROR("Load Image ERROR");
-      return -1;
+      ROS_ERROR("ERROR");
+      return -1;	 
    }else{
-      ROS_INFO("load Image success");
+      ROS_INFO("load success");
       return 0;
    }  
-}  
+}   
 
 int main(int argc, char **argv){
-   ros::init(argc, argv, "digital_signage");
+   ros::init(argc, argv, "image_viewer");
    ros::NodeHandle nh;
    DigitalSignage digital_signage(nh);
    if(digital_signage.check_image() == -1)
      return -1;
-
    // window作成
    cv::namedWindow("Image", CV_WINDOW_AUTOSIZE | CV_GUI_NORMAL);
    cv::moveWindow("Image", 0, 0);
